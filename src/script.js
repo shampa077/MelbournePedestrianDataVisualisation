0686c4b5f6acbcab778dd6ce3863a56ec672a2b3
Maxim Shishmarev <msgmaxim@gmail.com>
2017-02-02 09:21:13 +1100

    all code in js file

--- src/script.js
+++ src/script.js
@@ -56,63 +56,122 @@ function prepareDonutData(data) {
   /// for each hour figure out the min and the max
   for (let hour = 0; hour < 24; hour++) {
 
-    console.log(hourData[hour])
     const max = Math.max.apply(null, hourData[hour]);
     const min = Math.min.apply(null, hourData[hour]);
-    console.log(min)
-    console.log(max)
 
     result.mins.push(min);
     result.maxs.push(max);
   }
 
-  // console.log(result);
-  createDonut(result);
+  // createDonut(result);
+
+  // createMap(result)
 }
 
-function createDonut(hourData) {
+function createDonut(cx, cy, hourData) {
+
+    const svg = d3.select("svg");
     // dummy data to print a 'gear', doesn't really matter if min or max array is first
     var maxs = [40, 50, 40, 50, 40, 50, 40, 50, 40, 50, 90, 50, 40, 50, 40, 50, 40, 50, 40, 50, 40, 50, 40, 50];
     var mins = [0, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20];
     // var hourData = {maxs: maxs, mins: mins};
 
-    var donutCenterX = 200;
-    var donutCenterY = 200;
     var minimumDonutRadius = 70;
     var donutAngle = Math.PI; // will mostly likely just be Math.PI for semi-circle or 2*Math.PI for full circle;
 
 
     //"hot" half donut
 
-    var points = jaggedDonut(donutCenterX, donutCenterY, minimumDonutRadius, donutAngle, 1, hourData.maxs)
-                    .concat(jaggedDonut(donutCenterX, donutCenterY, minimumDonutRadius, donutAngle, 1, hourData.mins).reverse());
+    var points = jaggedDonut(cx, cy, minimumDonutRadius, donutAngle, 1, hourData.maxs)
+                    .concat(jaggedDonut(cx, cy, minimumDonutRadius, donutAngle, 1, hourData.mins).reverse());
 
     //we have to reverse the 'second' loop, whether that's min or max, as we are drawing a poly, going around once (e.g. clockwise), then looping back the other direction (e.g. counter-cw)
-    var canvas = d3.select("svg")
-    .append("svg:polygon")
-    .attr("fill", "red")
+    let frist_half = svg.append("svg:polygon")
+      .attr("fill", "red")
     //.attr("stroke", "black")
-    .attr("points", points);
+      .attr("points", points);
 
 
     //"cold" half donut
 
     //we have to reverse the 'second' loop, whether that's min or max, as we are drawing a poly, going around once (e.g. clockwise), then looping back the other direction (e.g. counter-cw)
-    var points = jaggedDonut(donutCenterX, donutCenterY, minimumDonutRadius, donutAngle, -1, hourData.maxs)
-                    .concat(jaggedDonut(donutCenterX, donutCenterY, minimumDonutRadius, donutAngle, -1, hourData.mins).reverse());
-    var canvas = d3.select("svg")
-    .append("svg:polygon")
-    .attr("fill", "blue")
+    var points = jaggedDonut(cx, cy, minimumDonutRadius, donutAngle, -1, hourData.maxs)
+                    .concat(jaggedDonut(cx, cy, minimumDonutRadius, donutAngle, -1, hourData.mins).reverse());
+
+    let second_half = svg.append("svg:polygon")
+      .attr("fill", "blue")
     //.attr("stroke", "black")
-    .attr("points", points);
+      .attr("points", points);
 
+    return [frist_half, second_half];
 
-    //reference circle in the middle
-
-    points = simpleCircle(200, 200, 70);
-    var canvas = d3.select("svg")
-    .append("svg:polygon")
-    .attr("fill", "grey")
-    //.attr("stroke", "black")
-    .attr("points", points);
 }
+
+  var donutMinimumRadius = 7;
+  var donutAngle = Math.PI; // will mostly likely just be Math.PI for semi-circle or 2*Math.PI for full circle;
+
+  L.mapbox.accessToken = 'pk.eyJ1IjoidmFoYW4iLCJhIjoiY2luaWhyaDBxMHdydHUybTMzanViNzJpNCJ9.B_ndOs4dnU_XghOU9xfnSg';
+  var map = L.mapbox.map('map', 'mapbox.streets',{ zoomControl:false, scrollWheelZoom :false })
+    .setView([-37.8148798759503,144.965210438559], 15);
+  
+  map.touchZoom.disable();
+  map.doubleClickZoom.disable();
+  map.scrollWheelZoom.disable();
+  map.boxZoom.disable();
+  map.keyboard.disable();
+  map.dragging.disable();
+  
+  
+  var svg = d3.select(map.getPanes().overlayPane).append("svg").attr("width", map._size.x).attr("height", map._size.y),
+    g = svg.append("g").attr("class", "leaflet-zoom-hide");
+  
+  
+  var data = [
+    { "coords" : [ -37.815145 , 144.966777]},
+    { "coords" : [ -37.811123 , 144.962733]}
+  ].map( function(d){ var newPoint = map.latLngToLayerPoint( d.coords ); d.coords = { 'x' : newPoint.x, 'y' : newPoint.y }; return d; } )
+  
+
+  g.selectAll("circle").data( data ).enter().append("circle")
+    .attr("cx", function(d){ console.log(d); return d.coords.x } )
+    .attr("cy", function(d){ return d.coords.y } )
+    .attr("r", 10 )
+    .style("fill", "red")
+    .on("mouseover", function(d, i) {
+        
+        var s = d3.select(this);
+        var marker = [];
+
+        //this will need to be replaced to get the data from this data point
+        var maxs = [40, 50, 40, 50, 40, 50, 40, 50, 40, 50, 90, 50, 40, 50, 40, 50, 40, 50, 40, 50, 40, 50, 40, 50];
+        var mins = [0, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20];
+        var hourData = {maxs: maxs, mins: mins};
+
+        let svgs = createDonut(+s.attr("cx"), +s.attr("cy"), hourData);
+
+        // var points = jaggedDonut(+s.attr("cx"), +s.attr("cy"), donutMinimumRadius, donutAngle, 1, hourData[0])
+        //         .concat(jaggedDonut(+s.attr("cx"), +s.attr("cy"), donutMinimumRadius, donutAngle, 1, hourData[1]).reverse());
+        
+        // marker.push(svg.append("svg:polygon")
+        // .attr("fill", "transparent")
+        // .attr("stroke", "red")
+        // .attr("points", points));
+
+        // //this will need to be replaced to get the data from this data point
+        // var maxs = [40, 50, 40, 50, 40, 50, 40, 50, 40, 50, 90, 50, 40, 50, 40, 50, 40, 50, 40, 50, 40, 50, 40, 50];
+        // var mins = [0, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20, 10, 20];
+        // var hourData = [maxs, mins];
+
+        // var points = jaggedDonut(+s.attr("cx"), +s.attr("cy"), donutMinimumRadius, donutAngle, -1, hourData[0])
+        //         .concat(jaggedDonut(+s.attr("cx"), +s.attr("cy"), donutMinimumRadius, donutAngle, -1, hourData[1]).reverse());
+        
+        // marker.push(svg.append("svg:polygon")
+        // .attr("fill", "transparent")
+        // .attr("stroke", "blue")
+        // .attr("points", points))
+
+        svg.selectAll("polygon").on("mouseout", function(d, i) {
+            svgs[0].remove();
+            svgs[1].remove();
+        });
+    })
